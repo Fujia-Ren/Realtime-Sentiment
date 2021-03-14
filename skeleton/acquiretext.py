c6e59f685d79daf5f9fafbcfb12afde3dad97655
Fujia Ren <fujiaren@Fujias-iMac.local>
2021-03-13 21:34:14 -0500

    added to the acquiretext skeleton. Added class structure, and changed old methods to class methods

--- skeleton/acquiretext.py
+++ skeleton/acquiretext.py
@@ -1,33 +1,49 @@
+class Tweet(object):
+	"""Tweet object:
+	self.text (str): content of the tweet
+	self.hashtags (lst(str)): list of hashtags in the tweet
+	self.time (str): timestamp of when the tweet was posted
+	self.user_id (str): the Twitter user_id of the poster	
+	"""
 
+	#https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+
+    def __init__(self, raw_tweet):
+		text, hashtags, time, user_id = self._format(raw_tweet)
+        self.text = text
+        self.hashtags = hashtags
+        self.time = time
+        self.user_id = user_id
+
+    def _format(self, raw_tweet):
+		"""Private method. Outputs tuple of (text, hashtags, time, user_id)"""
+		#formats it into the fields we want. Check example json. 
 
 def get_tweets():
     """
-      get_text from the twitch api
+    get_text from the twitch api
     """
-    example = [
-        {
-            "text": "Look at our planet earth",
-            "hashtags": ["earth", "life"],
-            "time": 2020,
-            "user_id": "globelover"
-        },
-        {
-            "text": "Qb4 Yay",
-            "hashtags": ["chess", "gm"],
-            "time": 2019,
-            "user_id": "chesscracker"
-        }
-    ]
+    # example = [
+    #     {
+    #         "text": "Look at our planet earth",
+    #         "hashtags": ["earth", "life"],
+    #         "time": 2020,
+    #         "user_id": "globelover"
+    #     },
+    #     {
+    #         "text": "Qb4 Yay",
+    #         "hashtags": ["chess", "gm"],
+    #         "time": 2019,
+    #         "user_id": "chesscracker"
+    #     }
+    # ]
+	
+
+	##Connect to Twitter in stream. 
+	for raw_tweet in stream:
+		new_tweet = Tweet(raw_tweet)
+		example.append(raw_tweet)
     return example
-
-
-def extract_texts(tweets):
-    """
-      extract texts from the tweets, in the text form we use for our CosmosDB
-      TEXT DATA FORM SHOULD BE CONSISTENT WITH analyzetext.py
-    """
-    placeholder = ""
-    return placeholder
 
 
 def send_to_db(texts):
@@ -43,5 +59,5 @@ if __name__ == "__main__":
     while(True):
       # For every min/hour/day (doesn't matter)
         tweets = get_tweets()
-        texts = extract_texts(tweets)
-        send_to_db(texts)
+		for tweet in tweets:
+        	send_to_db(tweet.text)
