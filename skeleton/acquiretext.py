9713a61d1f8320f50bb5bfa571a646102245d0d2
Fujia Ren <fujiaren@Fujias-iMac.local>
2021-03-14 18:27:00 -0400

    final cleanup

--- skeleton/acquiretext.py
+++ skeleton/acquiretext.py
@@ -8,20 +8,22 @@ class Tweet(object):
 
     # https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
     def __init__(self, raw_tweet):
-        text, hashtags, time, user_id = self._format(raw_tweet)
+        text, hashtags, time, user_id = self._parse(raw_tweet)
         self.text = text
         self.hashtags = hashtags
         self.time = time
         self.user_id = user_id
 
-    def _format(self, raw_tweet):
+    def _parse(self, raw_tweet):
         """Private method. Outputs tuple of (text, hashtags, time, user_id)"""
         # formats it into the fields we want. Check example json.
 
+    def format_db_entry(self):
+        """Public function. Will make Tweet object into form of db entry. @TODO decide on exactly what that is"""
 
 def get_tweets():
     """
-    get_text from the twitch api
+    get raw tweets in stream from twitter api. Will function as connection method.
     """
 
     # Connect to Twitter in stream.
@@ -45,4 +47,5 @@ if __name__ == "__main__":
         # For every min/hour/day (doesn't matter)
         tweets = get_tweets()
         for tweet in tweets:
-            send_to_db(tweet.text)
+            #note that tweet is a Tweet object
+            send_to_db(tweet.format_db_entry())
