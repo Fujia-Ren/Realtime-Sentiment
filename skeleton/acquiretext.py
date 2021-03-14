bcee9aa691f6266478bc4a07fc0cd3c1e7fd4e4a
Fujia Ren <fujiaren@Fujias-iMac.local>
2021-03-13 21:39:53 -0500

    fixed indentation inconsistencies

--- skeleton/acquiretext.py
+++ skeleton/acquiretext.py
@@ -7,15 +7,14 @@ class Tweet(object):
 	"""
 
 	#https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
-
-    def __init__(self, raw_tweet):
+	def __init__(self, raw_tweet):
 		text, hashtags, time, user_id = self._format(raw_tweet)
-        self.text = text
-        self.hashtags = hashtags
-        self.time = time
-        self.user_id = user_id
+		self.text = text
+		self.hashtags = hashtags
+		self.time = time
+		self.user_id = user_id
 
-    def _format(self, raw_tweet):
+	def _format(self, raw_tweet):
 		"""Private method. Outputs tuple of (text, hashtags, time, user_id)"""
 		#formats it into the fields we want. Check example json. 
 
@@ -43,7 +42,7 @@ def get_tweets():
 	for raw_tweet in stream:
 		new_tweet = Tweet(raw_tweet)
 		example.append(raw_tweet)
-    return example
+	return example
 
 
 def send_to_db(texts):
@@ -56,8 +55,8 @@ def send_to_db(texts):
 
 if __name__ == "__main__":
 
-    while(True):
-      # For every min/hour/day (doesn't matter)
-        tweets = get_tweets()
+	while(True):
+		# For every min/hour/day (doesn't matter)
+		tweets = get_tweets()
 		for tweet in tweets:
-        	send_to_db(tweet.text)
+			send_to_db(tweet.text)
