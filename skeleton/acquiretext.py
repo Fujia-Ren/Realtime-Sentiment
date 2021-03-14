e652ee02bd39f8e1a70f0e344b1f8aaa76294a21
Fujia Ren <fujiaren@Fujias-iMac.local>
2021-03-13 21:47:49 -0500

    changed it to spaces

--- skeleton/acquiretext.py
+++ skeleton/acquiretext.py
@@ -1,22 +1,22 @@
 class Tweet(object):
-	"""Tweet object:
-	self.text (str): content of the tweet
-	self.hashtags (lst(str)): list of hashtags in the tweet
-	self.time (str): timestamp of when the tweet was posted
-	self.user_id (str): the Twitter user_id of the poster	
-	"""
+    """Tweet object:
+    self.text (str): content of the tweet
+    self.hashtags (lst(str)): list of hashtags in the tweet
+    self.time (str): timestamp of when the tweet was posted
+    self.user_id (str): the Twitter user_id of the poster	
+    """
 
-	#https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
-	def __init__(self, raw_tweet):
-		text, hashtags, time, user_id = self._format(raw_tweet)
-		self.text = text
-		self.hashtags = hashtags
-		self.time = time
-		self.user_id = user_id
+    #https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+    def __init__(self, raw_tweet):
+        text, hashtags, time, user_id = self._format(raw_tweet)
+        self.text = text
+        self.hashtags = hashtags
+        self.time = time
+        self.user_id = user_id
 
-	def _format(self, raw_tweet):
-		"""Private method. Outputs tuple of (text, hashtags, time, user_id)"""
-		#formats it into the fields we want. Check example json. 
+    def _format(self, raw_tweet):
+        """Private method. Outputs tuple of (text, hashtags, time, user_id)"""
+        #formats it into the fields we want. Check example json. 
 
 def get_tweets():
     """
@@ -36,13 +36,13 @@ def get_tweets():
     #         "user_id": "chesscracker"
     #     }
     # ]
-	
+    
 
-	##Connect to Twitter in stream. 
-	for raw_tweet in stream:
-		new_tweet = Tweet(raw_tweet)
-		example.append(raw_tweet)
-	return example
+    ##Connect to Twitter in stream. 
+    for raw_tweet in stream:
+        new_tweet = Tweet(raw_tweet)
+        example.append(raw_tweet)
+    return example
 
 
 def send_to_db(texts):
@@ -55,8 +55,8 @@ def send_to_db(texts):
 
 if __name__ == "__main__":
 
-	while(True):
-		# For every min/hour/day (doesn't matter)
-		tweets = get_tweets()
-		for tweet in tweets:
-			send_to_db(tweet.text)
+    while(True):
+        # For every min/hour/day (doesn't matter)
+        tweets = get_tweets()
+        for tweet in tweets:
+            send_to_db(tweet.text)
