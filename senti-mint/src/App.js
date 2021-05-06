8a093b9a21375df1e25c75040ea948fdba4b2392
Fujia Ren <fujiaren@Fujias-iMac.local>
2021-05-06 00:28:56 -0400

    spicy micey frontend azure function that takes a query from db and displays

--- senti-mint/src/App.js
+++ senti-mint/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+function MostFrequent(){
+  return axios
+    .get('http://localhost:7071/api/Frontend?id=display_frequent&type=mylife')
+    .then((response) => response.data);
+}
 
 function App() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+      MostFrequent().then(data => setData(data));
+  }, []);
+
+  const dataSection = data 
+    ? <ul>{data.map(item => <li>{item}</li>)}</ul>
+    : <div id="data">Loading...</div>
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Most Frequent Hashtags:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {dataSection}
+
       </header>
     </div>
   );
